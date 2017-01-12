a631344b4547fcf43cc35a9e5cae58692bf84630
saideep <deepu.sai88@gmail.com>
2017-01-12 13:26:09 +0530

    first commit

--- MeetUpDEmo/ViewController.swift
+++ MeetUpDEmo/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         eventTable.dataSource = self
         
    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
